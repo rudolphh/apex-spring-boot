8ca6fd443ccbed2ef31f4fb821f85bcfb1ea2e82
alidad <programmer@dorisa.com>
2021-09-30 17:32:57 -0700

    initial state

---
+++ src/main/java/com/apex/eqp/inventory/helpers/ProductFilter.java
@@ -0,0 +1,27 @@
+package com.apex.eqp.inventory.helpers;
+
+import com.apex.eqp.inventory.entities.Product;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ProductFilter {
+
+    private final List<String> recalledProducts;
+
+    public ProductFilter(List<String> recalledProducts) {
+        if (recalledProducts == null) recalledProducts = new ArrayList<>();
+
+        this.recalledProducts = recalledProducts;
+    }
+
+    public List<Product> removeRecalled(Collection<Product> allProduct) {
+        return allProduct.stream().filter(ProductFilter::filterByName).collect(Collectors.toList());
+    }
+
+    private static boolean filterByName(Product product) {
+        return true;
+    }
+}
